431e2937dd7da777e03d370d1fa69ddb085d126c
GBCMENEZES <gibran.menezes@philips.com>
2022-03-21 14:01:08 -0300

    Studies about functions

--- JavaScript-Basics/teste.js
+++ JavaScript-Basics/teste.js
@@ -1,6 +1,34 @@
-const meuObjeto = {
-  chave: "valor"
- }
- 
- console.log(meuObjeto.chave); 
- //console.log(meuObjeto[chave]);
+// const user = {
+//   id: 42,
+//   displayName: 'jdoe',
+//   fullName: {
+//     firstName: 'John',
+//     lastName: 'Doe'
+//   }
+// };
+
+// function userId({id}){
+//   return id;
+// }
+
+// function getFullName({fullName: {firstName: first, lastName: last}}){
+//   return `${first} ${last}`;
+// }
+
+// console.log(userId(user)); //42
+// console.log(getFullName(user)); //John Doe
+
+// const person = {
+//   firstName: "Gibran",
+//   lastName: "Menezes",
+//   id: 1,
+//   fullName: function() {
+//     return this.firstName + " " + this.lastName;
+//   },
+//   getId: function() {
+//     return this.id;
+//   }
+// }
+
+// console.log(person.fullName());
+// console.log(person.getId());
