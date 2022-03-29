b95e6a2be464756ded22f357f86ff09f8ddddfa1
unknown <gibranquimica@gmail.com>
2022-03-28 21:14:16 -0300

    About Assync in js

--- JavaScript-Basics/assicronicidade/js/assync.js
+++ JavaScript-Basics/assicronicidade/js/assync.js
@@ -4,14 +4,12 @@ const catImg = document.getElementById('cat');
 
 
 const getCats = async () => {
-    try {
-        const data = await fetch(BASE_URL);
-        const json = await data.json();
-        return json.webpurl;
-    }
-    catch(e) {
-        console.log(e.message);
-    }
+    const data = await fetch(BASE_URL)
+    .then(res => res.json())
+    .catch(e => console.log(e))
+    
+    return data.webpurl;
+    
 };
 
 const loadImg = async () => {
